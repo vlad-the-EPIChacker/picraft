be121822ff6fddfe5a3a53d9dd3a0222c5393d8b
vpupkin <vpupkin8899@gmail.com>
2014-06-19 04:48:16 +0000

    building

--- blockfighter.py
+++ blockfighter.py
@@ -1,19 +1,21 @@
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create()
 import time
-time.sleep(60)
-points = 0
+while True:
+    time.sleep(10)
+    points = 0
 
-hits = mc.events.pollBlockHits()
+    hits = mc.events.pollBlockHits()
 
 
 
-for hit in hits:
-    x=hit.pos.x
-    y=hit.pos.y
-    z=hit.pos.z
-    block =mc.getBlock(x, y, z)
-    points =points + block
+    for hit in hits:
+        x=hit.pos.x
+        y=hit.pos.y
+        z=hit.pos.z
+        block =mc.getBlock(x, y, z)
+        points =points + block
+        mc.postToChat("You got " +str(block) + " points.")
     
                             
-mc.postToChat("You got " +str(points) + " points.")
+
--- city.py
+++ city.py
@@ -5,24 +5,35 @@ block = 1
 air = 0
 stone=1
 wood=5
-glass=20
-door=324
+glass=102
+door=64
 
 def clear(x, z, x2, z2):
     mc.setBlocks(x,0,z,x2,55,z2,air)
 
-def floor(x, z, w, len):
+def floor(x, z, w, len,i):
     h=5
-    mc.setBlocks(x,0,z,x+w,h,z+len,stone)#body#
-    mc.setBlocks(x+1,0+1,z+1,x+w-1,h-1,z+len-1,air)#hollow#
-    mc.setBlocks(x+1,0,z+1,x+w-1,0,z+len-1,wood)  #floor#
-    mc.setBlocks(x+w/2-1,h/2,z,x+w/2,h/2+1,z,glass)#window#
-    mc.setBlocks(x,h/2,z+len/2-1,x,h/2+1,z+len/2,glass)#window#
-    mc.setBlocks(x+w/2-1,h/2,z+w,x+w/2,h/2+1,z+w,door)#window#
-    mc.setBlocks(x+len,h/2,z+len/2-1,x+len,h/2+1,z+len/2,glass)#window#
+    hi=i*h
+    mc.setBlocks(x,0+hi,z,x+w,h+hi,z+len,stone)#body#
+    mc.setBlocks(x+1,0+1+hi,z+1,x+w-1,h-1+hi,z+len-1,air)#hollow#
+    mc.setBlocks(x+1,0+hi,z+1,x+w-1,0+hi,z+len-1,wood)  #floor#
+    mc.setBlocks(x+w/2-1,h/2+hi,z,x+w/2,h/2+1+hi,z,glass)#window#
+    mc.setBlocks(x,h/2+hi,z+len/2-1,x,h/2+1+hi,z+len/2,glass)#window#
+    mc.setBlock(x+w/2-1,h/2+hi-1,z+w,door,6)#door#
+    mc.setBlock(x+w/2-1,h/2+hi,z+w,door,6)#door#
+    mc.setBlocks(x+len,h/2+hi,z+len/2-1,x+len,h/2+1+hi,z+len/2,glass)#window#
+def building(x,z,f):
+    w=10
+    len=10
 
 
-######################Main######################
+
+    for i in range(0,f):
+        floor(20,20,w,len,i)
+    return
+    
+
+######################Main###############a######
     
 
 
@@ -32,6 +43,12 @@ z= 48.6
 x2= 49
 y2= 50
 z2= 25
+i=0
+
+
 #clear(x, z, x2, z2)#
 clear(0, 0, 50, 55)
-floor(20,20,10,10)
+#floor(20,20,10,10)#
+building(20,20,5)
+
+
