b45356ef7922a10f671c4db1750776902ba7ef9f
vpupkin <vpupkin8899@gmail.com>
2014-07-01 04:18:54 +0000

    fixed windows

--- city.py
+++ city.py
@@ -19,17 +19,20 @@ def floor(x, z, w, len,i):
     mc.setBlocks(x,0+hi,z,x+w,h+hi,z+len,stone)#body#
     mc.setBlocks(x+1,0+1+hi,z+1,x+w-1,h-1+hi,z+len-1,air)#hollow#
     mc.setBlocks(x+1,0+hi,z+1,x+w-1,0+hi,z+len-1,wood)  #floor#
-    mc.setBlocks(x+w/2-1,h/2+hi,z,x+w/2,h/2+1+hi,z,glass)#window#
-    mc.setBlocks(x,h/2+hi,z+len/2-1,x,h/2+1+hi,z+len/2,glass)#window#
-    mc.setBlock(x+w/2-1,h/2+hi-1,z+w,door,6)#door#
-    mc.setBlock(x+w/2-1,h/2+hi,z+w,door,6)#door#
-    mc.setBlocks(x+len,h/2+hi,z+len/2-1,x+len,h/2+1+hi,z+len/2,glass)#window#
+    mc.setBlocks(x+w/2,h/2+hi,z,x+w/2+1,h/2+1+hi,z,glass)#window#  z
+    mc.setBlocks(x,h/2+hi,z+len/2,x,h/2+1+hi,z+len/2+1,glass)#window# x
+    
+    
+    if i==0:
+        mc.setBlock(x+w/2,h/2+hi-1,z+len,door,6)#door#
+        mc.setBlock(x+w/2,h/2+hi,z+len,door,6)#door#
+    else:       
+        mc.setBlocks(x+w/2+1,h/2+hi,z+len,x+w/2,h/2+1+hi,z+len,glass)#window# z
+        
+    mc.setBlocks(x+w,h/2+hi,z+len/2,x+w,h/2+1+hi,z+len/2+1,glass)#window# x
 def building(x,z,f):
-    w=10
-    len=10
-
-
-
+    w=12
+    len=14
     for i in range(0,f):
         floor(x,z,w,len,i)
     return
